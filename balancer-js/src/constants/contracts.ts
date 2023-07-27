81100ec84510f045be5ab53bb75375a2c27e59de
Chimpy Developer <joaoc@d3vs.xyz>
2023-07-27 20:52:25 +0100

    Add Vault Binance Smart Chain

--- balancer-js/src/constants/contracts.ts
+++ balancer-js/src/constants/contracts.ts
@@ -1,3 +1,4 @@
 export const balancerVaultMap: { [network: number]: string } = {
     42161: '0x82A8d8B59a13eD9df879C1f450a379182661AB59',
+    56: '0x36B129B35Ac950b15558973fc246121923E3fD63'
 };
--- dist/index.umd.js
+++ dist/index.umd.js
@@ -766,6 +766,7 @@
 
     const balancerVaultMap = {
 		42161: '0x82A8d8B59a13eD9df879C1f450a379182661AB59',
+		56: '0x36B129B35Ac950b15558973fc246121923E3fD63'
     };
 
     /*
