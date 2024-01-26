74c3be72b0e4a466b67dbee4ce30f9f12f30da0f
Chimpy Developer <joaoc@d3vs.xyz>
2024-01-26 17:07:43 -0300

    Update with the addresses of client 2

--- balancer-js/src/constants/contracts.ts
+++ balancer-js/src/constants/contracts.ts
@@ -1,5 +1,5 @@
 export const balancerVaultMap: { [network: number]: string } = {
     42161: '0x82A8d8B59a13eD9df879C1f450a379182661AB59',
     56: '0x24562f9f35aA8416Fd6A819eeCD20531abB4cD5D',
-    137: '0xBC77248Ded7b5C1Cd32B0Ce1533421c332BBfa0b'
+    137: '0x809797847Cf39cbB49ffc060A7aC9FAAB61baAb6'
 };
